e722ccebd9b27763c705af1160a9bec22916afed
BlankGodd <blankgodd33@gmail.com>
2020-07-14 20:08:17 +0100

    server

--- server.py
+++ server.py
@@ -1,29 +1,69 @@
 #!usr/bin/python
 # Author:   @BlankGodd_
 
-import socket
-import ast
-import threading
-import mod
+import select
+import tincanchat
+from types import SimpleNamespace
+from collections import deque
 
-HOST = ''
-PORT = 4200
+HOST = mod.HOST
+PORT = mod.PORT
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-sock.bind((HOST, PORT))
-sock.listen(100)
+clients = {}
 
-addr = sock.getsockname()
-print('Listening on {}'.format(addr))
-print()
+def create_client(sock):
+    return SimpleNamespace(sock=sock,rest=bytes(),
+                           send_queue=deque())
 
-while True:
-    client_sock, addr = sock.accept()
-    thread = threading.Thread(target=mod.handle_client,
-                              args=[client_sock,addr],
-                              daemon=True)
-    thread.start()
-    print('Connection from {}'.format(addr))
+def broadcast_msg(msg):
+    data = tincanchat.prep_msg(msg)
+    for client in clients.values():
+        client.send_queue.append(data)
+        poll.register(client.sock, select.POLLOUT)
 
+if __name__ == '__main__':
+    listen_sock = tincanchat.create_listen_socket(HOST, PORT)
+    poll = select.poll()
+    poll.register(listen_sock, select.POLLIN)
+    addr = listen_sock.getsockname()
+    print('Listening on {}'.format(addr))
+
+    while True:
+        for fd, event in poll.poll():
+            if event & (select.POLLHUP | select.POLLERR |
+                select.POLLNVAL):
+                poll.unregister(fd)
+                del clients[fd]
+                
+            elif fd == listen_sock.fileno():
+                client_sock,addr = listen_sock.accept()
+                client_sock.setblocking(False)
+                fd = client_sock.fileno()
+                clients[fd] = create_client(client_sock)
+                poll.register(fd, select.POLLIN)
+                print('Connection from {}'.format(addr))
+
+            elif event & select.POLLIN:
+                client = clients[fd]
+                addr = client.sock.getpeername()
+                recvd = client.sock.recv(4096)
+                if not recvd:
+                    client.sock.close()
+                    print('Client {} disconnected'.format(addr))
+                    continue
+                data = client.rest + recvd
+                (msgs, client.rest) = tincanchat.parse_recvd_data(data)
+                for msg in msgs:
+                    msg = '{}: {}'.format(addr, msg)
+                    print(msg)
+                    broadcast_msg(msg)
+
+            elif event & select.POLLOUT:
+                client = clients[fd]
+                data = client.send_queue.popleft()
+                sent = client.sock.send(data)
+                if sent < len(data):
+                    client.sends.appendleft(data[sent:])
+                if not client.send_queue:
+                    poll.modify(client.sock, select.POLLIN)
 
